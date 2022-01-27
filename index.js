012cdb726bc05f5cc285782df86ab7b3a19a69b9
Fabian Gruber <fabian.gruber93@gmx.de>
2022-01-27 14:04:10 +0100

    Add second counter

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const textShow = 'show answer';
 
 showAnswerButton?.addEventListener('click', () => {
   // ternary operator
-  showAnswerButton.textContent = showAnswerButton.textContent === textShow ? textHide : textShow;
+  showAnswerButton.textContent =
+    showAnswerButton.textContent === textShow ? textHide : textShow;
 });
 
 // bookmark
@@ -18,7 +19,7 @@ let cardIsBookmarked = false;
 
 cardBookmark?.addEventListener('click', () => {
   // if (!cardIsBookmarked) {
-  //   cardBookmark.src = '../../images/bookmark 20x20px.svg';
+  //   cardBookmark.src = '../../images/bookmark-20x20px.svg';
   //   cardIsBookmarked = true;
   // } else {
   //   cardBookmark.src = '../../images/no-bookmark.svg';
@@ -27,21 +28,30 @@ cardBookmark?.addEventListener('click', () => {
 
   cardBookmark.src = cardIsBookmarked
     ? '../../images/no-bookmark.svg'
-    : '../../images/bookmark 20x20px.svg';
+    : '../../images/bookmark-20x20px.svg';
   cardIsBookmarked = !cardIsBookmarked;
 });
 
+// create
 //counter Add Question
 
 const addQuestionInput = document.querySelector(
   '[data-js="create__add-question"]'
 );
 const addQuestionOutput = document.querySelector(
-  '[data-js="create__question-output"]'
+  '[data-js="create__add-question-output"]'
 );
-const counter = 200;
+const counter = 250;
 
 addQuestionInput.addEventListener('input', () => {
   const length = addQuestionInput.value.length;
-  addQuestionOutput.value = counter - length;
+  addQuestionOutput.value = counter - length + " sign's left";
 });
+
+
+// counnter add answer
+const addAnswerInput = document.querySelector('[data-js="create__add-anwser"]');
+const addAnswerOutput = document.querySelector('[data-js="create__add-anwser-output"]')
+addAnswerInput.addEventListener('input', () => {
+
+})
