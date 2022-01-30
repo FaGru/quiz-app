7f074c728aa0185bf478c35b9b6732e1b36e50f3
Fabian Gruber <fabian.gruber93@gmx.de>
2022-01-30 16:33:49 +0100

    Add navigation with one html

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import Navigation from './js/Navigation.js';
 import Card from './js/Card.js';
 import Form from './js/Form.js';
+
+const allNavElements = document.querySelectorAll('[data-js="nav"]');
+
+allNavElements.forEach(navElement => {
+  Navigation(navElement);
+});
+
 
 const allCardElements = document.querySelectorAll('[data-js="card"]');
 
---
+++ js/Navigation.js
@@ -0,0 +1,47 @@
+export default function Navigation(navElement) {
+  const buttonHome = navElement.querySelector(
+    '[data-js="nav__home-button"]'
+  );
+  const buttonBookmark = navElement.querySelector(
+    '[data-js="nav__bookmark-button"]'
+  );
+  const buttonCreate = navElement.querySelector(
+    '[data-js="nav__create-button"]'
+  );
+  const buttonProfil = navElement.querySelector(
+    '[data-js="nav__profil-button"]'
+  );
+  const pageHome = document.querySelector('[data-js="home"]');
+  const pageBookmark = document.querySelector('[data-js="bookmark"]');
+  const pageCreate = document.querySelector('[data-js="create"]');
+  const pageProfil = document.querySelector('[data-js="profil"]');
+
+
+  buttonHome.addEventListener('click', () => {
+    pageHome.classList.remove('hidden');
+    pageBookmark.classList.add('hidden');
+    pageCreate.classList.add('hidden');
+    pageProfil.classList.add('hidden')
+  });
+
+  buttonBookmark.addEventListener('click', () => {
+    pageHome.classList.add('hidden');
+    pageBookmark.classList.remove('hidden');
+    pageCreate.classList.add('hidden');
+    pageProfil.classList.add('hidden')
+  });
+
+  buttonCreate.addEventListener('click', () => {
+    pageHome.classList.add('hidden');
+    pageBookmark.classList.add('hidden');
+    pageCreate.classList.remove('hidden');
+    pageProfil.classList.add('hidden')
+  });
+
+  buttonProfil.addEventListener('click', () => {
+    pageHome.classList.add('hidden');
+    pageBookmark.classList.add('hidden');
+    pageCreate.classList.add('hidden');
+    pageProfil.classList.remove('hidden')
+  });
+}
